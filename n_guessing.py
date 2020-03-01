eb4cfc45e954ab073444d8cd03c9cfd3d10a39ee
Terence Wang <terence79119@gmail.com>
2020-03-01 17:37:51 +0800

    update

--- n_guessing.py
+++ n_guessing.py
@@ -2,16 +2,18 @@ import random
 
 r = random.randint(1, 100)
 r = int(r)
-
+x = 1
 while True:
 	ipt = input('請輸入一個數字: ')
 	ipt = int(ipt)
 	if ipt == r:
 		print('你答對了!')
+		print('你總共猜了', x ,'次')
 		break
-	else:
-		if ipt > r:
-			print('你輸入的數字比答案大喔')
-		else:
-			print('你輸入的數字比答案小喔')
+	elif ipt > r:
+		print('你輸入的數字比答案大喔')
+		x = x + 1
+	elif ipt < r:
+		print('你輸入的數字比答案小喔')
+		x = x + 1
 
